101f82c3c4b0db998ec315998f820878460b4933
ralaonit <60430975+ralaonit@users.noreply.github.com>
2020-12-29 16:35:02 +0100

    Add files via upload
    nuevos cambios para probar

--- primer_node.js
+++ primer_node.js
@@ -1,6 +1,11 @@
+var hola = "hola";
+var adios = "adios";
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World! Hola Mundo');
+  res.end('Hello World! ');
+
+// Tercer cambio pongo una variable inutil al princpio
+
 }).listen(8080);
